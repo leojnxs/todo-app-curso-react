27b22e1342078b8e32bb08d7f9713f3449965e86
Leonardo Andrade <Leonardo Andrade>
2017-03-28 15:32:27 -0300

    Component todoForm, add and change events

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,26 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.state = { description: '', list: [] }
+    }
+
+    handleAdd() {
+        // TODO: integration with backend
+    }
+
+    handleChange(event) {
+        this.setState({ ...this.state, description: event.target.value })
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <TodoForm />
+                <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange} />
                 <TodoList />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
 export default props => (
-    <div>
-        <h1>form</h1>
+    <div role="form" className="todoForm">
+        <div className="col-xs-11 col-sm-11 col-md-11">
+            <input type="text" id="description" className="form-control" placeholder="Add task" value={props.description} onChange={props.handleChange} />
+        </div>
+        <div className="col-xs-1 col-sm-1 col-md-1">
+            <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
+        </div>
     </div>
 )
